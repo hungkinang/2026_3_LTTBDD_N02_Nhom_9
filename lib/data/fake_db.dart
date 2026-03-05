5221f082eddf78cc938e2be93f1801aa90ae345c
Minh Đức <151888529+DucZaki@users.noreply.github.com>
2026-03-05 21:54:26 +0700

    update

--- lib/data/fake_db.dart
+++ lib/data/fake_db.dart
@@ -5,5 +5,24 @@ class FakeDB {
   static List<UserModel> users = [
     UserModel(email: "admin@gmail.com", password: "123456"),
   ];
-  static List<HistoryModel> histories = [];
+  static List<HistoryModel> histories = [
+    HistoryModel(
+      title: "Toán Cơ Bản",
+      score: 8,
+      total: 10,
+      date: "12/03/2022",
+    ),
+    HistoryModel(
+      title: "Ngữ Pháp Tiếng Anh",
+      score: 7,
+      total: 10,
+      date: "10/03/2022",
+    ),
+    HistoryModel(
+      title: "Vật Lý Lớp 10",
+      score: 9,
+      total: 10,
+      date: "08/03/2022",
+    ),
+  ];
 }
